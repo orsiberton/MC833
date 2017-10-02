ead7656acafc961c6dfbf1a8eac556eac73b3f4f
bberton-ciandt <bberton@ciandt.com>
2017-10-02 20:42:58 -0300

    adding base project for 4.2

---
+++ exercicio_4.2/my_socket_api.h
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <time.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
+#define MAXLINE 4096
+#define LISTENQ 5
+
+/*
+  Define um typo booleano
+*/
+typedef enum {
+  FALSE = 0,
+  TRUE = 1
+} bool;
+
+/*
+  Funções que abstraem a interface de sockets
+*/
+int Socket(int family, int type, int flags);
+void Connect(int socket, const struct sockaddr *sockaddr, socklen_t sockaddr_len);
+void Bind(int socket, const struct sockaddr *sockaddr, socklen_t sockaddr_len);
+void Listen(int socket, int queue_size);
+int Accept(int socket, struct sockaddr *sockaddr, socklen_t *sockaddr_len);
+
+/*
+  Funções auxiliares
+*/
+bool isExit(const char *message);
+pid_t Fork();
+void PrintClientData(const struct sockaddr_in *sockaddr, char *clientName, int clientName_len);
+void PrintData(int socket, const struct sockaddr_in *sockaddr, char *localHost, char *localPort);

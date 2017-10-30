fe9052e5dd81e113a5c266c94f55f4d00194ba7b
bberton-ciandt <bberton@ciandt.com>
2017-10-30 21:16:57 -0200

    melhorias

--- exercicio_6/servidor.c
+++ exercicio_6/servidor.c
@@ -6,30 +6,23 @@ int main (int argc, char **argv) {
    struct sockaddr_in servaddr, clientaddr;
    char buf[MAXLINE];
    fd_set rset;
-   FILE *log;
 
    // verifica se o nome do arquivo foi passado por parametro
    if (argc != 3) {
       perror("Porta/Log nao informado!");
       exit(0);
    }
-   
-   port = atoi(argv[1]);
-   log = fopen(argv[2], "w");
 
-   if(log == NULL){
-      perror("Error creating log");
-      exit(0);
-   }
+   port = atoi(argv[1]);
 
    // cria um socket TCP
    listenfd = Socket(AF_INET, SOCK_STREAM, 0);
-   
+
    //type of socket created
     servaddr.sin_family = AF_INET;
     servaddr.sin_addr.s_addr = INADDR_ANY;
     servaddr.sin_port = htons(port);
-   
+
    // faz o bind do socket TCP com o host:porta escolhidos
    Bind(listenfd, (struct sockaddr *)&servaddr, sizeof(servaddr));
 
@@ -40,42 +33,35 @@ int main (int argc, char **argv) {
 
    // espera por conexões de clientes indefinidamente
    for ( ; ; ) {
-
       FD_SET(listenfd, &rset);
-      
       maxfdp1 = listenfd;
-      
       activity = select(maxfdp1 + 1, &rset , NULL , NULL , NULL);
-      
+
       if ((activity < 0) && (errno!=EINTR)) {
-            printf("select error");
+          perror("select error");
+          exit(1);
       }
 
       if ((new_socket = accept(listenfd, (struct sockaddr *)&clientaddr, (socklen_t*)&clientaddr_len))<0){
-	   perror("accept");
-	   exit(EXIT_FAILURE);
+	       perror("accept error");
+	       exit(1);
       }
 
-	while (FD_ISSET(listenfd, &rset)) {
-		if ((n = read(new_socket, buf, MAXLINE)) < 0) {
-		       perror("read error");
-		       exit(1);
-     		}
+    	while (FD_ISSET(listenfd, &rset)) {
+    		if ((n = read(new_socket, buf, MAXLINE)) < 0) {
+    		    perror("read error");
+    		    exit(1);
+        }
 
-                fprintf(log, "%s", buf);
-		
-		if( send(new_socket, buf, strlen(buf), 0) != strlen(buf) ) {
-	                perror("send");
-		}
-
-	}
-
+    		if (send(new_socket, buf, strlen(buf), 0) != strlen(buf) ) {
+    	     perror("send error");
+           exit(1);
+    		}
+    	}
    }
 
    // fecha arquivo de log
    close(listenfd);
-   fclose(log);
 
    return(0);
 }
-
